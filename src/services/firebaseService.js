6b623bdee102e81721a6a73327ea44ed2c771bb6
P52 <dangminhquan9320@gmail.com>
2025-12-28 23:39:06 +0700

    feat/update .env

--- src/services/firebaseService.js
+++ src/services/firebaseService.js
@@ -18,13 +18,13 @@ import toastService from './toastService';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAsXppxDg_gcAJJdK08KFyooW0TSxwaQa8",
-  authDomain: "nglieu-67e5b.firebaseapp.com",
-  projectId: "nglieu-67e5b",
-  storageBucket: "nglieu-67e5b.firebasestorage.app",
-  messagingSenderId: "937625471592",
-  appId: "1:937625471592:web:20de36d7463fa0558c09dd",
-  measurementId: "G-QCL0KWRTTG"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
